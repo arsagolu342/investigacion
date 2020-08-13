096b42a6868ae6ea45bc77457066a06838ca375f
arsagolu342 <arielgordillolucas@gmail.com>
2020-08-13 14:55:14 -0500

    Hoja de vida Ariel .

---
+++ ariel.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ariel </title>
+</head>
+<body>
+    <h1> Ariel Gordillo</h1>
+    <p> Domicilio: Latacunga <br>
+        Edad: 19 <br>
+        Hobbies: Dibujar y Pintar
+     </p>
+</body>
+</html>
