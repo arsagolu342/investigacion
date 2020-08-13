ae878daaa287ee5ca52dfec03862d05b42ae5fb4
samiel1410 <69322868+samiel1410@users.noreply.github.com>
2020-08-13 14:53:28 -0500

    Hoja de Vida Samiel

---
+++ samiel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Samiel Morejon</title>
+</head>
+<body>
+<h1>Samiel Morejon</h1>
+<p>Domicilio:Latacunga <br> Edad:20 años <br> Hobbies: Videojuegos</p>
+    
+</body>
+</html>
